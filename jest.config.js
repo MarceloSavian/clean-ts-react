8794231206f59ee4f78581b490c7aede73b58584
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-14 18:30:47 -0300

    chore: add webpack configs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts, tsx}'
+    '<rootDir>/src/**/*.{ts, tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   },
   transform: {
     '.+\\.{ts|tsx}$': 'ts-jest'
